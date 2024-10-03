d4b82371fc94db917268b9449d3499da02b8c926
georgi4511 <14124374+georgi4511@users.noreply.github.com>
2024-10-04 00:21:16 +0300

    go to sleep me

--- src/main/java/com/github/georgi4511/discord_bot/procedures/LoggingProcedure.java
+++ src/main/java/com/github/georgi4511/discord_bot/procedures/LoggingProcedure.java
@@ -1,24 +1,24 @@
 package com.github.georgi4511.discord_bot.procedures;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
-import java.time.LocalTime;
+import java.time.Instant;
 
 @Slf4j
 @NoArgsConstructor
 @Service
+@Getter
 public class LoggingProcedure {
 
-   private static final LocalTime startTime = LocalTime.now();
+   private static final long startTime = Instant.now().getEpochSecond();
 
     @Scheduled(fixedRateString = "${procedure.rate}")
     @Async
     public void log() {
-        log.info("Bot has been alive for {} seconds", Duration.between(startTime,LocalTime.now()).getSeconds());
+        log.info("Bot has been alive for {} seconds", Instant.now().getEpochSecond() - startTime);
     }
 }
